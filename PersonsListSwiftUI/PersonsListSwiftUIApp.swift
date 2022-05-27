e48ee0ae5b38a12210b187453da5207ff84c36cd
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-27 16:40:04 +0500

    Initial Commit

---
+++ PersonsListSwiftUI/PersonsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonsListSwiftUIApp.swift
+//  PersonsListSwiftUI
+//
+//  Created by Aleksandr Kretov on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
